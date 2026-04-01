e8fdbf87b566e47c9f4d7279a1502a2a8b56ceeb
William Mossberg <william.mossberg@outlook.com>
2026-04-01 15:46:44 +0200

    Added command line arguments

--- src/main.c
+++ src/main.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+
 #include "filter.h"
 
 #define STB_IMAGE_IMPLEMENTATION
@@ -6,23 +10,59 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-int main() {
-    const char* infile = "images/in-test.jpg";
-    const char* outfile = "images/out-test.jpg";
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        goto usage;
+    }
+
+    char* infile = argv[1];
+
+    optind = 2;
+    int opt;
+    char* outfile = NULL;
+    char* filter = NULL;
+    bool overwrite = false;
+
+    while ((opt = getopt(argc, argv, "f:o:y")) != -1) {
+        switch (opt) {
+            case 'f': filter = optarg; break;
+            case 'o': outfile = optarg; break;
+            case 'y': overwrite = true; break;
+            case '?': goto usage;
+        }
+    }
 
     int width, height, channels;
     unsigned char* data = stbi_load(infile, &width, &height, &channels, 0);
     if (!data) {
-        return -1;
+        return 1;
     }
 
-    grayscale(data, width, height, channels);
+    if (filter) {
+        if (strcmp(filter, "grayscale") == 0)
+            grayscale(data, width, height, channels);
+    }
 
-    printf("width: %d, height: %d, channels: %d\n", width, height, channels);
+    if (!outfile) {
+        outfile = infile;
+        if (!overwrite) {
+            printf("No output file specified, overwrite '%s'? [y/N] ", infile);
+            char ch = getchar();
+            if (ch != 'y' && ch != 'Y') {
+                printf("Aborted.\n");
+                stbi_image_free(data);
+                return 0;
+            }
+        }
+    }
 
     stbi_write_jpg(outfile, width, height, channels, data, 100);
 
     stbi_image_free(data);
 
     return 0;
+
+usage: 
+    fprintf(stderr, "Usage: %s <infile> [options]\n", argv[0]);
+    return 1;
 }
